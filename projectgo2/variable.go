1c686bc795286eec654279f7efd814aaf949138a
Karan Soin <155584509+ksoin96@users.noreply.github.com>
2026-03-04 00:29:38 -0500

    Create variable.go with variable examples
    Add variable declaration examples and print sum

---
+++ projectgo2/variable.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var x int = 10
+	y := 20
+	// two different ways of declaring variables. First mean x is an integer with value of 10.
+	//The second has y wiht va;ue of integer 20 through inference
+	fmt.Println(x + y)
+}
