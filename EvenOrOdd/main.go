3c7cdb235fffa770734bc286d1b8c44001e8610b
Karan Soin <155584509+ksoin96@users.noreply.github.com>
2026-03-05 09:50:16 -0500

    Create EvenOrOdd/main.go

---
+++ EvenOrOdd/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    var x int
+    fmt.Print("Enter your number: ")
+    fmt.Scan(&x)
+
+    if x%2 == 0 {
+        fmt.Println("Even")
+    } else {
+        fmt.Println("Odd")
+    }
+}
